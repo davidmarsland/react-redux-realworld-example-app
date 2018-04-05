c8fc81a439f12446af465058f42a4bc1af700546
David Marsland <14043765+davidmarsland@users.noreply.github.com>
2018-04-05 14:37:28 -0700

    added Redux DEVTOOLS

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const defaultState = {
 const reducer = function(state = defaultState, action) {
   return state;
 };
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render((
   <Provider store={store}>
